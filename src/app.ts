dcf1dd34f2e6e2dcd089570a9b1003ebc8e2e6e0
Selva Ganesh M <selva.ganesh@ogi-voos.com>
2025-05-24 21:03:29 +0530

    upg: init fixes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
-import express from "express"
-const app=express()
+import express, { Request, Response } from "express"
+import morgan from "morgan"
+import { config } from "./config";
+import shared from "@brace-for-impact/bfi-shared"
 
-app.use(express.json())
+const app=express()
+const port=process.env.PORT || 3000
+
+app.get("/api/health", (req: Request, res: Response) => {
+    res.status(200).json({
+        status: "ok",
+        message: "Auth Service is running",
+        data: shared?.config?.testServices?.testConfigService()
+    })
+})
 
 export default app
