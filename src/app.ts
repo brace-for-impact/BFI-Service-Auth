98335c131e05408238452e32e74e93bc6c53439a
abijith <abijithsurendran005@gmail.com>
2025-05-25 00:24:37 +0530

    api logger implemented

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express, { Request, Response } from "express"
-import morgan from "morgan"
-import { config } from "./config";
 import shared from "@brace-for-impact/bfi-shared"
 
 const app=express()
-const port=process.env.PORT || 3000
+
+app.use(shared.middlewares.apiLogger({
+    logHttpMethod: true,
+    logRequestUrl: true,
+    logRequestBody: true,
+    logResponseTime: true,
+    logStatusCode: true,
+  }))
 
 app.get("/api/health", (req: Request, res: Response) => {
     res.status(200).json({
         status: "ok",
         message: "Auth Service is running",
-        data: shared?.config?.testServices?.testConfigService()
     })
 })
 
